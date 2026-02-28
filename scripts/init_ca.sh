2256ab8460bfcf2880b36aa2940998c90d6ff1e3
Gabin-Crn <gabincornaire@outlook.fr>
2026-02-28 17:36:02 +1100

    Updating init_ca -> Editing folder creation, editing folder architecture

--- scripts/init_ca.sh
+++ scripts/init_ca.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail 
 
+
 # Variables 
 
 PKI_DIR="pki"
 PKI_CA="${PKI_DIR}/ca"
-PKI_CERTS="${PKI_CA}/certs"
-PKI_CRL="${PKI_CA}/crl"
-PKI_PRIVATE="${PKI_CA}/private"
-PKI_NEWCERTS="${PKI_CA}/newcerts"
-PKI_SERIAL="${PKI_CA}/serial"
-PKI_INDEX="${PKI_CA}/index.txt"
-PKI_CRL_NUMBER="${PKI_CA}/crl_number"
+PKI_ISSUE="${PKI_DIR}/issue"
+
 
 OPENSSL_CONFIG="openssl.cnf"
 
@@ -20,36 +16,38 @@ OPENSSL_CONFIG="openssl.cnf"
 if [ -d "pki" ]; then
     echo "[+] pki directory already exists"
 else
-    mkdir -p "${PKI_CA}" "${PKI_NEWCERTS}" "${PKI_PRIVATE}" "${PKI_CERTS}" "${PKI_CRL}"
-    echo "[+] Creation (CA -> newcerts, private, certs, crl) directories"
-    touch "${PKI_INDEX}"
-    echo 1000 > ${PKI_SERIAL}
-    echo 1000 > ${PKI_CRL_NUMBER}
+    mkdir -p "${PKI_CA}" "${PKI_CA}/newcerts" "${PKI_CA}/private" "${PKI_CA}/certs" "${PKI_CA}/crl" 
+    mkdir -p "${PKI_ISSUE}" "${PKI_ISSUE}/newcerts" "${PKI_ISSUE}/private" "${PKI_ISSUE}/certs" "${PKI_ISSUE}/crl"
+    mkdir -p "${PKI_DIR}/csr"
+    echo "[+] Creation (CA -> newcerts, private, certs, crl, csr) directories"
+    touch "${PKI_CA}/index.txt"
+    echo 1000 > "${PKI_CA}/crlnumber", "${PKI_ISSUE}/crlnumber"
+    echo 1000 > "${PKI_CA}/serial", "${PKI_ISSUE}/serial"
     echo "[+] Creation index.txt, serial, crl_number files & initial values for serial and crl_number"  
 fi
 
 # RootCA Key generation
 
-if [ -f "${PKI_PRIVATE}/root-ca.key" ]; then
+if [ -f "${PKI_CA}/private/root-ca.key" ]; then
     echo "[+] Root CA key already exists"
 else
-    openssl genrsa -aes256 -out "${PKI_PRIVATE}/root-ca.key" 4096
+    openssl genrsa -aes256 -out "${PKI_CA}/private/root-ca.key" 4096
     echo "[+] Root CA key generated"
 fi
 
 
 # RootCA Certificate generation
 
-if [ -f "${PKI_CERTS}/root-ca.crt" ]; then
+if [ -f "${PKI_CA}/certs/root-ca.crt" ]; then
     echo "[+] Root CA certificate already exists"
 else
-    openssl req -new -x509 -config ${OPENSSL_CONFIG} -key ${PKI_PRIVATE}/root-ca.key -out ${PKI_CERTS}/root-ca.crt -days 7300
+    openssl req -new -x509 -config ${OPENSSL_CONFIG} -key ${PKI_CA}/private/root-ca.key -out ${PKI_CA}/certs/root-ca.crt -days 7300
     echo "[+] Root CA certificate generated"
 fi
 
 # Permission management 
-chmod 700 "${PKI_PRIVATE}"
-chmod 400 "${PKI_PRIVATE}/root-ca.key"
+chmod 700 "${PKI_CA}/private"
+chmod 400 "${PKI_CA}/private/root-ca.key"
 
-echo "Private directory permission: $(ls -ld "${PKI_PRIVATE}")"
-echo "Root-ca key permission: $(ls -l "${PKI_PRIVATE}/root-ca.key")"
+echo "Private directory permission: $(ls -ld "${PKI_CA}/private")"
+echo "Root-ca key permission: $(ls -l "${PKI_CA}/private/root-ca.key")"
--- scripts/issue_certs.sh
+++ scripts/issue_certs.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 PKI_DIR="pki"
 PKI_CSR="${PKI_DIR}/ca/csr"
+PKI_CERT="${PKI_DIR}/ca/certs"
 PKI_PRIVATE="${PKI_DIR}/ca/private"
-
+NUMBER-OF-DRONES=$1
 
 ## Check if files exists
-
 
 # Issue key generation
 if [[  -f "${PKI/PRIVATE}/issue.key" ]]; then
@@ -20,7 +20,7 @@ else
 fi
 
 # Drone key generation
-for (( i=0; i<3; i++)); do 
+for (( i=0; i<$1; i++)); do 
     if [[ -f "${PKI/PRIVATE}/drones.${i}.key"]]; then
         echo "[+] Drone ${i} already exists"
     else
@@ -40,8 +40,8 @@ else
     echo "[+] Issue csr has been generated"
 fi
 
-for ((i=0; i<3; i++)); do
-    if [[ -f "${PKI_CSR}/drone.${i}"]]; then
+for ((i=0; i<$1; i++)); do
+    if [[ -f "${PKI_CSR}/drone.${i}.csr"]]; then
         echo "[+] Drone ${i} csr already exists"
     else
         openssl -req -new -config "${OPENSSL_CONFIG}" -key "${PKI_PRIVATE}/drone-${i}.key" -out "${PKI_CSR}/drone-${i}.csr"
@@ -50,5 +50,22 @@ for ((i=0; i<3; i++)); do
 done
 
 
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Issue + Drones certificate generation
+#------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
+if [[ -f "${PKI_CERTS}/issue.crt" ]]; then
+    echo "[+] Issue cert already exists"
+else
+    openssl x509 -req -in "${$PKI_CSR}/issue.csr" -signkey "${PKI_PRIVATE}/root-ca.key" -out "${PKI_CERTS}/issue.crt"
+    echo "[+] Issue cert has been generated"
+fi
+
+for (( i=0; i < $1; i++)); do
+    if [[ -f "${PKI_CERTS}/drone.${i}.crt"]]; then
+        echo "[+] Drone-${i} already exists"
+    else
+        openssl x509 -req -in "${PKI_CSR}/drone-${i}.csr" -signkey "${PKI_PRIVATE}/issue.key" -out "${PKI_CERTS}/drone-${i}.crt"
+        echo "[+] Drone-${i} has been generated"
+done
